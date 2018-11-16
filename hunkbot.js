0fea58fd869dc1617bf3fbd3ef25a6bc8769668a
Justin Cameron <justin.b.cameron@gmail.com>
2018-11-16 00:44:44 -0500

    Started working on handling case where count of unread items is greater than zero. TODO: Move everything into functions and then figure out async so you can make sure things complete in the correct order.

--- hunkbot.js
+++ hunkbot.js
@@ -5,6 +5,8 @@ const HUNKBOTPASSWORD = process.env.HUNKBOTPASSWORD
 var loginResponse
 var authenticationToken
 var isUnreadResponse
+var isUnreadFlag
+var activityResponse
 
 // 1. Login
 
@@ -46,11 +48,41 @@ request(loginOptions, function (error, response, body) {
     request(isUnreadOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         isUnreadResponse = body
-        let isUnreadFlag = JSON.parse(isUnreadResponse).data.isUnreadActivity
+        isUnreadFlag = JSON.parse(isUnreadResponse).data.isUnreadActivity
         console.log(`isUnreadFlag: ${isUnreadFlag}`)
         if (isUnreadFlag === true) console.log('Unread items exist.')
         else console.log('No unread items exist.')
       }
     })
+
+    // 3. Retrieve activity if unread items exist (and if there are unread items, handle them here??)
+    if (isUnreadFlag === true) {
+      // do something in reply to each unread mention
+      // activity: Configure headers
+      var activityHeaders = {
+        'Authorization': `Bearer ${authenticationToken}`
+      }
+
+      // activity: Request details
+      var activityOptions = {
+        url: 'https://v1.peachapi.com/activity',
+        method: 'GET',
+        headers: activityHeaders
+      }
+
+      // Check to see if there are any unread items in our activity stream
+      request(activityOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          activityResponse = body
+          // isUnreadFlag = JSON.parse(isUnreadResponse).data.isUnreadActivity
+          console.log(`activityResponse: ${activityResponse}`)
+          // if (isUnreadFlag === true) console.log('Unread items exist.')
+          // else console.log('No unread items exist.')
+        }
+      })
+    } else {
+      // do something/nothing when there are no unread items
+      console.log('No unread items to act on.')
+    }
   }
 })
